aaa4f20bbd4d34634d8009cb1d6e74ba87b900dc
Ryan Blakeman <rblakeman31@gmail.com>
2021-03-17 00:21:53 -0700

    added timestamp log for deploy verification

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,18 @@ import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log('last updated: March 17, 2021');
+    }
     render() {
         return (
             <div>
                 <SearchBar />
                 <WeatherList />
             </div>
-        //<Chart />
+            //<Chart />
         );
     }
 }
